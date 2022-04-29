3fef651763a54331fcd63f9563f9f9ce59d30fb8
Maximilien Dagois <mdagois@google.com>
2022-04-30 00:34:17 +0900

    CGBCONV
    - Added arguments parsing
    - Added PNG functions
    - Added color conversion functions

--- tools/conv/cgbconv.cpp
+++ tools/conv/cgbconv.cpp
@@ -1,17 +1,49 @@
+#include <cassert>
 #include <cstdint>
+#include <iostream>
+#include <png.h>
+#include <vector>
+
+using namespace std;
+
+////////////////////////////////////////////////////////////////////////////////
+// Constants
+////////////////////////////////////////////////////////////////////////////////
 
 enum : uint32_t
 {
+	kTileSize = 8,
+	kRowsPerTile = 8,
 	kColorsPerPalette = 4,
 	kPaletteMaxCount = 8,
-	kRowsPerTile = 8,
 };
+
+////////////////////////////////////////////////////////////////////////////////
+// Color
+////////////////////////////////////////////////////////////////////////////////
+
+struct ColorRGBA
+{
+	union
+	{
+		uint32_t rgba;
+		struct
+		{
+			uint8_t r;
+			uint8_t g;
+			uint8_t b;
+			uint8_t a;
+		};
+	};
+};
+
+typedef uint16_t ColorBGR555;
 
 struct Palette
 {
 	union
 	{
-		uint16_t colors[kColorsPerPalette];
+		ColorBGR555 colors[kColorsPerPalette];
 		uint64_t value;
 	};
 };
@@ -20,6 +52,100 @@ struct PaletteSet
 {
 	Palette palettes[kPaletteMaxCount];
 };
+
+static ColorBGR555 ConvertColor(ColorRGBA rgba)
+{
+	const uint8_t red = rgba.r / 8;
+	const uint8_t green = rgba.g / 8;
+	const uint8_t blue = rgba.b / 8;
+	assert(red < 32 && green < 32 && blue < 32);
+	return (blue << 10) | (green << 5) | red;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Image
+////////////////////////////////////////////////////////////////////////////////
+
+class Image
+{
+public:
+	Image();
+	~Image();
+
+	bool read(const char* filename);
+	bool validateSize() const;
+
+	uint32_t getWidth() const;
+	uint32_t getHeight() const;
+
+private:
+	png_image m_png;
+	union
+	{
+		png_bytep m_buffer;
+		ColorRGBA* m_pixels;
+	};
+};
+
+Image::Image()
+: m_buffer(nullptr)
+{
+	memset(&m_png, 0, sizeof(png_image));
+}
+
+Image::~Image()
+{
+	if(m_buffer)
+	{
+		free(m_buffer);
+	}
+	png_image_free(&m_png);
+}
+
+bool Image::read(const char* filename)
+{
+	assert(filename);
+
+	m_png.version = PNG_IMAGE_VERSION;
+	if(png_image_begin_read_from_file(&m_png, filename) == 0)
+	{
+		return false;
+	}
+
+	m_png.format = PNG_FORMAT_RGBA;
+	m_buffer = reinterpret_cast<png_bytep>(malloc(PNG_IMAGE_SIZE(m_png)));
+	if(m_buffer == nullptr)
+	{
+		png_image_free(&m_png);
+		return false;
+	}
+
+	if(png_image_finish_read(&m_png, nullptr, m_buffer, 0, nullptr) == 0)
+	{
+		free(m_buffer);
+		png_image_free(&m_png);
+		return false;
+	}
+
+	return true;
+}
+
+bool Image::validateSize() const
+{
+	return ((getWidth() % kTileSize) == 0) && ((getHeight() % kTileSize) == 0);
+}
+
+uint32_t Image::getWidth() const
+{
+	return m_png.width;
+}
+
+uint32_t Image::getHeight() const
+{
+	return m_png.height;
+}
+
+////////////////////////////////////////////////////////////////////////////////
 
 struct TileFlip
 {
@@ -44,9 +170,28 @@ struct Tile
 	TileFlip flips[kTileFlip_Count];
 };
 
+typedef vector<const char*> FileList;
+
 int main(int argc, const char** argv)
 {
-	// TODO Check CLI arguments (tileset.png + several tilemap.png)
+	if(argc < 2)
+	{
+		cout
+			<< "USAGE" << endl
+			<< argv[0] << " <tileset_png_filename> [tilemap_png ...]" << endl;
+		return 0;
+	}
+
+	const char* input_tileset_filename = argv[1];
+	FileList input_tilemap_filenames;
+	{
+		for(int32_t i = 2; i < argc; ++i)
+		{
+			input_tilemap_filenames.push_back(argv[i]);
+		}
+	}
+
+	PaletteSet palette_set = {};
 
 	// TODO Extract palettes (up to 8)
 	// - Build an ordered list of four BGRA555 colors for each tile
