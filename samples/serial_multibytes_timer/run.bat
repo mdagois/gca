5c1667a9fb08aa63a0aa944f7b97b86a1ceef05a
Maximilien Dagois <maximiliendagois@gmail.com>
2022-08-10 13:49:19 +0900

    Fixed serial run batch

--- samples/serial_multibytes_timer/run.bat
+++ samples/serial_multibytes_timer/run.bat
@@ -1,10 +1,12 @@
 echo off
 pushd "%~dp0"
 
+PORT=8765
+
 for %%i in (.) do set rom_name=%%~ni
 
-start /b bgb --watch --rom %rom_name%.gb --listen 8765
-start /b bgb --watch --rom %rom_name%.gb --connect 127.0.0.1
+start /b bgb --watch --rom %rom_name%.gb --listen %PORT%
+start /b bgb --watch --rom %rom_name%.gb --connect 127.0.0.1:%PORT%
 
 popd
 
