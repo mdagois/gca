14ea225181d29bfb69cd98f91eee73c24b7e3621
Maximilien Dagois <maximiliendagois@gmail.com>
2022-08-13 12:45:30 +0900

    Added assets for printer application

--- build_assets.bat
+++ build_assets.bat
@@ -29,13 +29,20 @@ set DMG_GENERATED_DIR=%GENERATED_DIR%dmg\
 set DMG_CHR_DIR=%DMG_GENERATED_DIR%%CHR_EXTENSION%\
 set DMG_TLM_DIR=%DMG_GENERATED_DIR%%TLM_EXTENSION%\
 
+:: general samples
 %DMG_CONV% %DMG_TILESET_DIR%tileset.png %DMG_TILEMAP_DIR%background.png %DMG_TILEMAP_DIR%ch1_continuous.png %DMG_TILEMAP_DIR%ch3_simple.png %DMG_TILEMAP_DIR%ch3_waveforms.png %DMG_TILEMAP_DIR%ch4_example.png %DMG_TILEMAP_DIR%counter_rng.png %DMG_TILEMAP_DIR%frame.png %DMG_TILEMAP_DIR%input_interrupt.png %DMG_TILEMAP_DIR%interrupt_trigger.png %DMG_TILEMAP_DIR%serial_multibytes.png %DMG_TILEMAP_DIR%serial_parent_child.png %DMG_TILEMAP_DIR%serial_switch_roles.png %DMG_TILEMAP_DIR%serial_transfer.png %DMG_TILEMAP_DIR%sound_duty.png %DMG_TILEMAP_DIR%sound_envelope.png %DMG_TILEMAP_DIR%sound_frequency.png %DMG_TILEMAP_DIR%sound_length.png %DMG_TILEMAP_DIR%sound_sweep.png %DMG_TILEMAP_DIR%sound_volume.png %DMG_TILEMAP_DIR%timer_sec.png %DMG_TILEMAP_DIR%window.png
 if %errorlevel% neq 0 goto end
 
+:: parallax sample
 %DMG_CONV% %DMG_TILESET_DIR%parallax.png %DMG_TILEMAP_DIR%parallax.png
 if %errorlevel% neq 0 goto end
 
+:: sound test
 %DMG_CONV% %DMG_TILESET_DIR%sound_test.png %DMG_TILEMAP_DIR%ch1_test.png %DMG_TILEMAP_DIR%ch2_test.png %DMG_TILEMAP_DIR%ch3_test.png %DMG_TILEMAP_DIR%ch4_test.png
+if %errorlevel% neq 0 goto end
+
+:: printer application
+%DMG_CONV% %DMG_TILESET_DIR%printer.png
 if %errorlevel% neq 0 goto end
 
 del /f /s /q %DMG_CHR_DIR% 2> nul 1> nul
