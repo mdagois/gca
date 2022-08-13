fe6323178f3a2e8e520ba340b9bf2d1277d10778
Maximilien Dagois <maximiliendagois@gmail.com>
2022-08-13 14:51:34 +0900

    Printer

---
+++ tools/printer/run.bat
@@ -0,0 +1,10 @@
+echo off
+pushd "%~dp0"
+
+for %%i in (.) do set rom_name=%%~ni
+
+start /b bgb --watch --rom %rom_name%.gb --listen 8765
+start /b bgb --watch --rom %rom_name%.gb --connect 127.0.0.1:8765
+
+popd
+
