8eadfe0f44fac21f0a83c7a75083e0b752287b2c
Maximilien Dagois <maximiliendagois@gmail.com>
2022-10-08 10:45:01 +0900

    Printer

--- samples/printer/run.bat
+++ samples/printer/run.bat
@@ -1,11 +1,13 @@
 echo off
 pushd "%~dp0"
 
+set simulator_rom=..\..\tools\printer\rom\debug\printer.gbc
+
 set PORT=8765
 
 for %%i in (.) do set rom_name=%%~ni
 
-start /b bgb --watch --rom simulator\printer.gbc --listen %PORT%
+start /b bgb --watch --rom %simulator_rom% --listen %PORT%
 start /b bgb --watch --rom %rom_name%.gb --connect 127.0.0.1:%PORT%
 
 popd
