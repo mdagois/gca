4348cd8e62f0c13dd13b9031b6da38e75ad57550
Maximilien <maximiliendagois@gmail.com>
2022-11-25 22:26:01 +0900

    SGB color

---
+++ samples/sgb_color_3/build.bat
@@ -0,0 +1,18 @@
+echo off
+pushd "%~dp0"
+
+for %%i in (.) do set rom_name=%%~ni
+
+rgbasm -Werror -Weverything -Hl -o main.o main.rgbasm
+if %errorlevel% neq 0 goto end
+rgbasm -Werror -Weverything -Hl -o sample.o sample.rgbasm
+if %errorlevel% neq 0 goto end
+rgblink --tiny --map %rom_name%.map --sym %rom_name%.sym -o %rom_name%.gb main.o sample.o
+if %errorlevel% neq 0 goto end
+rgbfix --title game --sgb-compatible --old-licensee 0x33 --pad-value 0 --validate %rom_name%.gb
+if %errorlevel% neq 0 goto end
+
+:end
+popd
+exit /b %errorlevel%
+
